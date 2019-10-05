b7570e9d0458a117b5dfbf7db8fe0493a4306320
Cristian Muriel <cristian.muriel.ordonez@omnisens.com>
2019-10-05 10:12:52 +0200

    Some Player Stuff

---
+++ Start With Nothing/Assets/PlayerAnimation.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+
+    private Animator m_PlayerAnimator;
+
+    private void Awake()
+    {
+        m_PlayerAnimator = GetComponent<Animator>();
+    }
+
+    public void Run()
+    {
+        m_PlayerAnimator.SetTrigger("Running");
+    }
+
+    public void Idle()
+    {
+        m_PlayerAnimator.SetTrigger("Idle");
+    }
+
+    public void FaceLeft()
+    {
+        this.transform.localScale = new Vector3(-1, transform.localScale.y, transform.localScale.z);
+    }
+
+    public void FaceRight()
+    {
+        this.transform.localScale = new Vector3(1, transform.localScale.y, transform.localScale.z);
+    }
+}
---
+++ Start With Nothing/Assets/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private PlayerAnimation m_PlayerAnimation;
+
+
+    private void Awake()
+    {
+        m_PlayerAnimation = GetComponentInChildren<PlayerAnimation>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            m_PlayerAnimation.Run();
+            m_PlayerAnimation.FaceRight();
+        }
+
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            m_PlayerAnimation.Idle();
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            m_PlayerAnimation.Run();
+            m_PlayerAnimation.FaceLeft();
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            m_PlayerAnimation.Idle();
+
+        }
+    }
+}
